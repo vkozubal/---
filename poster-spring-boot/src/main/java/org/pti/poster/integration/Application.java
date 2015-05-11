84b153ac3cd4e138c2ddfe360faec3faf3994cd9
vova <volodymyr.kozubal@goeuro.com>
2015-05-11 13:49:54 +0300

    adding hazelcast to share user session in spring security through several tomcat instances.

--- poster-spring-boot/src/main/java/org/pti/poster/integration/Application.java
+++ poster-spring-boot/src/main/java/org/pti/poster/integration/Application.java
@@ -1,22 +1,26 @@
 package org.pti.poster.integration;
 
 
+import com.hazelcast.web.SessionListener;
+import com.hazelcast.web.spring.SpringAwareWebFilter;
 import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.pti.poster.integration.security.WebSecurityConfiguration;
 import org.pti.poster.rest.RestSpringConfig;
 import org.pti.poster.service.SpringServiceApplicationConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+
+import javax.servlet.DispatcherType;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableWebSecurity
-@ComponentScan
 @EnableSwagger
 @Import(value = {SpringServiceApplicationConfiguration.class, WebSecurityConfiguration.class, RestSpringConfig.class})
 public class Application extends WebMvcAutoConfiguration {
@@ -25,4 +29,38 @@ public class Application extends WebMvcAutoConfiguration {
         SpringApplication.run(Application.class, args);
     }
 
+
+    /*  hazelcast configuration */
+    @Bean
+    public FilterRegistrationBean hazelcastFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean(new SpringAwareWebFilter());
+
+        registration.addUrlPatterns("/*");
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE);
+
+        // Configure init parameters as appropriate:
+        registration.addInitParameter("map-name", "sessions-map");
+        registration.addInitParameter("cookie-name", "hazelcast.sessionId");
+//         registration.addInitParameter("debug", "true");
+//         registration.addInitParameter("config-location", "/WEB-INF/hazelcast.xml");
+//         registration.addInitParameter("instance-name", "default-hazelcast-instance");
+
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<SessionListener> hazelcastSessionListener() {
+        return new ServletListenerRegistrationBean<SessionListener>(new SessionListener());
+    }
+
+    @Bean
+    /*in case of using hazelcast we have an exception like 
+    NoSuchBeanDefinitionException: No qualifying bean of type 
+    [org.springframework.security.core.session.SessionRegistry]
+    works fine now
+    */
+    
+    public SessionRegistry sessionRegistry() {
+        return new org.springframework.security.core.session.SessionRegistryImpl();
+    }
 }
